a97d7e2c9c8906f27ef3dcdf471f229cf8e22344
tygavin <tianyao@zju.edu.cn>
2019-06-27 04:38:46 +0800

    connect with ethereum's smart contract

--- src/components/Rawdata.vue
+++ src/components/Rawdata.vue
@@ -1,28 +1,35 @@
 <template>
 <div class="container">
-  <div class="hello"> 
+  <div class="con2"> 
     <div class="terminal"> 
         <div class="terminal_text" id="terminal_text">
-           {{data}}
         </div>
-
-        <div class="terminal_text2" id="terminal_text">
+        <div style="width:20px"></div>
+        <div class="terminal_text2" id="terminal_text2">
         </div>
     </div>
     
-    <div>
-      <div class="device"></div>
-      <div class="gaia_ok"></div>
-      <div class="gaia_upload"></div>  
-      <div class="eth"></div> 
+    <div class="con3" v-if="false">
+      <div class="con2r" style="width:33%">
+          <div class="device pic"></div>
+      </div>
+      <div class="con2r" style="width:33%">
+          <div class="gaia_ok pic" v-if="upload"></div>
+          <div class="gaia_upload pic" v-if="!upload"></div>
+      </div>
+      <div class="con2r" style="width:33%">
+          <div class="eth pic"></div>
+      </div>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+var crypto = require('crypto');
 var senRawData = require('./SensorData.json')
 var senData = senRawData.data
+console.log(web3)
 console.log(senData.length)
 export default {
 
@@ -32,17 +39,20 @@ export default {
     return {
       timer:"",
       count: 0,
-      data:''
+      data:'',
+      upload:false,
+      timer2:""
     }
   },
   methods: {
     sensorInput(){
       console.log(this.count)
       var t = senData[this.count].sensorSteam
+      this.data += t
+      //console.log(t)
       var textdiv = document.getElementById("terminal_text");
       var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/g;
       var url = t.match(reg);
-      console.log(url);
       if (url!=null){
         textdiv.innerHTML += '<a target="_blank" href="'+t+'">'+senData[this.count].sensorSteam+'</a><br>';
       }
@@ -54,9 +64,154 @@ export default {
       //this.data += senData[this.count].sensorSteam + ' \n '
       this.count = this.count + 1
       if (this.count == senData.length) this.count = 0
+      
+      
     },
+    fingerprint(){
+      var md5 = crypto.createHash('md5');
+      var result = md5.update(this.data);
+
+      var t = result.digest("hex");
+      console.log(t);
+      var textdiv = document.getElementById("terminal_text2");
+      textdiv.innerHTML += t + '<br>';
+      textdiv.scrollTop = textdiv.scrollHeight;
+      var ContractABI = web3.eth.contract([
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "newHash",
+              "type": "string"
+            }
+          ],
+          "name": "recordHash",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "newOwner",
+              "type": "address"
+            }
+          ],
+          "name": "transferOwnership",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "name": "index",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "name": "recordedHash",
+              "type": "string"
+            }
+          ],
+          "name": "RecordHash",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "name": "previousOwner",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "name": "newOwner",
+              "type": "address"
+            }
+          ],
+          "name": "OwnershipTransferred",
+          "type": "event"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "owner",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "recordNum",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "records",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ]);
+    var Contract = ContractABI.at('0xd92aac5e0bf1116ac366d590329459eab3679671');
+    Contract.recordHash(t, 
+        	function(err, res){
+        		if (!err) alert("transaction success");
+        		else alert("transaction falied");
+  			});
+
+
+    
+    }
+    ,
     ssync(){
       this.timer = setInterval(this.sensorInput, 1000);
+      this.timer2 = setInterval(this.fingerprint , 30000);
     }
 
   },
@@ -69,49 +224,63 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .container{
-  display: block;
+  width: 80%;
 }
-.hello {
+
+.con2 {
   display:flex;
   flex-direction: column;
   height:100%;
   justify-content: center;
   align-content: center;
+  padding:100px 0 0 0;
+}
+
+.con2r {
+  display:flex;
+  height:100%;
+  justify-content: center;
+  align-content: center;
+}
+
+.con3 {
+  display:flex;
+  height:100%;
 }
 
 .terminal{
     border-style:solid;
     width: 100%;
-    height: 200px;
-    border-width: 2px 0px 0px 0px;
-    border-color: rgb(150,173,189);
+    height: 300px;
+    border-width: 1px 1px 1px 1px;
+    border-color: rgb(249,246,239);
     display:flex;
     justify-content: center;
     align-content: center;
 }
 .terminal_text2{
     overflow-y:scroll;
-    background-color: rgb(150,173,189);
+    background-color: rgb(249,246,239);
     caret-color: white;
-    text-shadow: 0px 0px 0px rgb(87, 80, 124);
+    text-shadow: 0px 0px 0px rgb(253,97,87) ;
     -webkit-text-fill-color: transparent;
     font-size: 14px;
     font-weight: 500;
     letter-spacing:1px;
-    padding:10px 10px 10px 10px;
+    padding:10px 0px 0px 10px;
     border-style: none;
     width:30%
 }
 .terminal_text{
     overflow-y:scroll;
-    background-color: rgb(150,173,189);
+    background-color: rgb(249,246,239);
     caret-color: white;
-    text-shadow: 0px 0px 0px rgb(87, 80, 124);
+    text-shadow: 0px 0px 0px rgb(253,97,87) ;
     -webkit-text-fill-color: transparent;
     font-size: 14px;
     font-weight: 500;
     letter-spacing:1px;
-    padding:10px 10px 10px 10px;
+    padding:10px 0px 0px 10px;
     border-style: none;
     width:70%;
     scrollbar-base-color:#ff6600;
@@ -120,29 +289,24 @@ export default {
 
 .gaia_ok{
     background: url(../assets/gaia_ok.png);
-    height:100px;
-    width:100px;
-    background-size:100% 100%;
 }
 
 .gaia_upload{
     background: url(../assets/gaia_upload.png);
-    height:100px;
-    width:100px;
-    background-size:100% 100%;
 }
 
 .eth{
     background: url(../assets/eth.png);
-    height:100px;
-    width:100px;
-    background-size:100% 100%;
 }
 
 .device{
     background: url(../assets/device.png);
-    height:100px;
-    width:100px;
+    
+}
+
+.pic{
+    height:150px;
+    width:150px;
     background-size:100% 100%;
 }
 
