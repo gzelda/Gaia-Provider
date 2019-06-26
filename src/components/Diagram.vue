6d90200c419ab08cd506eb40974f5283afbb69d8
tygavin <tianyao@zju.edu.cn>
2019-06-26 23:02:25 +0800

    update Rawdata

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="block">
-    
-    <transition name="" appear
-                  @before-enter="handleBeforeEnter"
-                  @enter="handleEnter"
-                  @after-enter="handleAfterEnter"
-                  @before-leave="handleBeforeLeave"
-                  @leave="handleLeave"
-                  @after-leave="handleAfterLeave"
-                  :css="false">
-        <div class="circle" v-if="show"></div>
-    </transition>
-    <div class="sensors">
-        <div v-for="(sensor,index) in sensors" :key="index" class="sensor">
-            <div class="circle"></div>
-        </div>
-    </div>
-    <div class="gaia">
-        <div class="gaiaCircle"></div>
-    </div>
+         <svg id="isvg" >
 
-    <div class="evm">
-        <div class="evmCircle"></div>
-    </div>
+<defs>
+
+<marker id="idArrow"
+
+         viewBox="0 0 20 20" refX="0" refY="10"
+
+         markerUnits="strokeWidth" markerWidth="3" markerHeight="10"
+
+         orient="auto">
+
+         <path d="M 0 0 L 20 10 L 0 20 z" fill="purple" stroke="black"/>
+
+      </marker>
+
+</defs>
+
+  <path d="M 50 450 l 0 -350"  stroke="blue"  stroke-width="3" fill="none" marker-end="url(#idArrow)" />
+
+  
+
+</svg>
   </div>
 </template>
 
@@ -33,108 +33,15 @@ export default {
   props: ['user'],
   data () {
     return {
-        show:false,
-        sensors:[1, 2, 3, 4, 5, 6, 7]
+       
     }
   },
   methods: {
-    hide(){
-      this.show = !this.show
-    },
-    handleBeforeEnter: function (el) {
-        el.style.opacity = 0;
-        console.log('方块显示动画即将执行');
-    },
-    handleEnter: function (el, done) {
-      Velocity(el, 'stop');
-      Velocity(el, {
-        backgroundColor: '#ffffff',
-        opacity: 1,
-        translateX: 260,
-        translateY: 100
-      }, {
-        duration: 1000,
-        complete: done
-      });
-      console.log('方块显示动画执行中...');
-    },
-    handleAfterEnter: function (el) {
-      console.log('方块显示动画结束');
-    },
-    handleBeforeLeave: function (el) {
-      console.log('方块隐藏动画即将执行');
-    },
-    handleLeave: function (el, done) {
-      Velocity(el, 'stop');
-      Velocity(el, {
-        backgroundColor: '#ffffff',
-        opacity: 0,
-        translateX: 0
-      }, {
-        duration: 1000,
-        complete: done
-      });
-      console.log('方块隐藏动画执行中...');
-    },
-    handleAfterLeave: function (el) {
-      console.log('方块隐藏动画结束');
-    }
+    
   }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.block{
-            display: flex;
-            padding: 20px 20px 20px 20px;
-            height: 100%;
-        }
-.circle {
-			background: #ffffff;
-			height: 20px;
-			width: 20px;			
-			border-radius: 10px;
-        }
 
-.gaiaCircle{
-    background: #ffffff;
-    border-radius: 50px;
-    height: 100px;
-	width: 100px;
-}
-
-.evmCircle{
-    background: #ffffff;
-    border-radius: 80px;
-    height: 160px;
-	width: 160px;
-}
-
-.sensors{   
-            display:flex;
-            flex-direction:column;
-            height: 100%;
-            width: 200px;
-            justify-content: center;
-            align-items: center;
-        }
-.sensor{
-    padding: 10px 10px 10px 10px;
-}
-.gaia{
-    
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width: 200px;
-    height:100%;
-}
-
-.evm{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width:50%;
-    height:100%;
-}
 </style>
--- src/components/Rawdata.vue
+++ src/components/Rawdata.vue
@@ -1,22 +1,56 @@
 <template>
   <div class="hello"> 
+    <div class="terminal"> 
+        <div class="terminal_text" id="terminal_text">
+           {{data}}
+        </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
-
-
+var senRawData = require('./SensorData.json')
+var senData = senRawData.data
+console.log(senData.length)
 export default {
-  
+
   name: 'rawdata',
   props: ['user'],
   data () {
     return {
-
+      timer:"",
+      count: 0,
+      data:''
     }
   },
   methods: {
-    
+    sensorInput(){
+      console.log(this.count)
+      var t = senData[this.count].sensorSteam
+      var textdiv = document.getElementById("terminal_text");
+      var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/g;
+      var url = t.match(reg);
+      console.log(url);
+      if (url!=null){
+        textdiv.innerHTML += '<a target="_blank" href="'+t+'">'+senData[this.count].sensorSteam+'</a><br>';
+      }
+      else{
+        textdiv.innerHTML += t + '<br>';
+      }
+      textdiv.scrollTop = textdiv.scrollHeight;
+
+      //this.data += senData[this.count].sensorSteam + ' \n '
+      this.count = this.count + 1
+      if (this.count == senData.length) this.count = 0
+    },
+    ssync(){
+      this.timer = setInterval(this.sensorInput, 1000);
+    }
+
+  },
+  mounted(){
+    this.ssync()
   }
 }
 </script>
@@ -24,7 +58,33 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .hello {
+  display:flex;
+  height:200px;
   justify-content: center;
+  align-content: center;
+}
+
+.terminal{
+    border-style:solid;
+    width: 600px;
+    border-width: 2px 0px 0px 0px;
+    border-color: rgb(24,26,31);
+    display:flex;
+}
+
+.terminal_text{
+    overflow:scroll;
+    background-color: rgb(33,37,43);
+    caret-color: white;
+    text-shadow: 0px 0px 0px #FFFFFF;
+    -webkit-text-fill-color: transparent;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing:1px;
+    padding:10px 10px 10px 10px;
+    flex :1;
+    border-style: none;
+    width:100%
 }
 
 
